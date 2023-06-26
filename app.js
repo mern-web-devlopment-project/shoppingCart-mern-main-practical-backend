be786ab4941503e2ab79dc3b96d1385923661aa2
nagaraj6618 <nagaraj516700@gmail.com>
2023-06-26 11:02:28 +0530

    Final commit

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const shoppingCartRouter = require('./routes/shoppingCartRoute')
+const mongoose = require('mongoose')
 const cors = require('cors')
-const PORT = 3500;
+const PORT = 3500
 
-app.get('/', (req,res) => {
-  res.send("Working")
-} )
-app.listen(PORT,() => console.log(`http://localhost:${PORT}`))
+mongoose.connect(process.env.DB_URL)
+const db = mongoose.connection
+db.on('error', (errorMessage) => console.log(errorMessage))
+db.once('open', () => console.log(`Connected to the database Successfully`))
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/v1/shoppingCart', shoppingCartRouter)
+
+app.listen(PORT, console.log(`Server started running at http://localhost:${PORT}/api/v1/shoppingCart/`))
